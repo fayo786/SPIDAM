e1da8ccf97320c3b6b7c375ca239ac65da11cf59
Melissa L. Fayo Guzman <107896893+fayo786@users.noreply.github.com>
2024-12-03 15:09:44 -0500

    View.py
    Added convert audio button

--- GUI.py
+++ GUI.py
@@ -3,4 +3,6 @@ import tkinter as tk
 
 #this is a simple window
 window = tk.Tk()
+Convert_Audio_Button = tk.Button(window, text = "Convert Audio")
+Convert_Audio_Button.grid(column = 1, row = 0, sticky = "ne", padx = 10, pady = 10)
 window.mainloop()
