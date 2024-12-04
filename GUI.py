35f3e1137e81bac3d67dc47728bd3f87cf582cd3
Melissa L. Fayo Guzman <107896893+fayo786@users.noreply.github.com>
2024-12-03 19:46:44 -0500

    GUI.py
    Fixed button placements

--- GUI.py
+++ GUI.py
@@ -1,16 +1,17 @@
 #importing library for the gui
 import tkinter as tk
-import matplotlib.pyplot as plt
-from tkinter import Text
-from tkinter import Label
-from tkinter import filedialog
-from tkinter import messagebox
+#import matplotlib.pyplot as plt
+#from tkinter import Text
+#from tkinter import Label
+#from tkinter import filedialog
+#from tkinter import messagebox
 
 #this is a simple window
 window = tk.Tk()
+#Convert Audio button
 Convert_Audio_Button = tk.Button(window, text = "Convert Audio")
-Convert_Audio_Button.grid(column = 1, row = 0, sticky = "ne", padx = 10, pady = 10)
-
+Convert_Audio_Button.grid(column = 2, row = 0, sticky = "ne", padx = 10, pady = 10)
+#Load audio button
 convert_load = tk.Button (window, text = "Load Audio")
-convert_load.grid(column=1, row=0, sticky = "ne", padx =10, pady=5)
+convert_load.grid(column=1, row=0, sticky = "ne", padx =10, pady=10)
 window.mainloop()
