a25c04c46b03c54864032342a0b12b1296b2e599
Melissa L. Fayo Guzman <107896893+fayo786@users.noreply.github.com>
2024-12-04 10:34:40 -0500

    main.py
    Running module-->(main.py)

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from GUI import GUI
+from controller import Controller
+
+from controller import Controller
+
+if __name__ == "__main__":
+    app = Controller()
+    app.run()
+
+
